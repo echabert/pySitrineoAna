eb8d2bfe760f5b6e881d1d1b9e76a52b7b75d52d
echabert <Eric.Chabert@cern.ch>
2020-07-16 18:51:57 +0200

    directory change

--- macros/AnaBeamSpot.py
+++ macros/AnaBeamSpot.py
@@ -2,7 +2,7 @@ import ROOT
 from ROOT import gROOT,TEllipse, TH1F, TH2F, TCanvas, TF1, TFile, TGraph, gApplication, TLegend
 import copy
 import math
-from pySitrineoAna.code.BeamSpot import BeamSpot
+from pySitrineoAna.base.BeamSpot import BeamSpot
 
 
 nxPixels = 960
